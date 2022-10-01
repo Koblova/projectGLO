6b781c0afc9bc9e83f9f95628ae3d61450ddabf9
Alina <alina.kirov@mail.ru>
2022-10-01 12:45:27 +0300

    готово

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "projectGLO";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 14;
+let rollback = 88;
+let fullPrice = 100500000;
+let adaptive = true;
 
-console.log("Привет, мир!");
-alert("Привет, мир!");
+console.log(title); //название проекта
+console.log(fullPrice); // з/п
+console.log(adaptive); //булевое
+
+console.log(screens.length); //длина строки
+
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей' + ';' + ' ' + 'Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей.'); //стоимость в рублях
+
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей' + ';' + ' ' + 'Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей.'); //стоимость в рублях
+
+console.log(screens.toLowerCase()); //нижний регистр
+console.log(screens.split(',')); //перевод в массив
+
+console.log(fullPrice * (rollback/100)); //процент отката
